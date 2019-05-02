b6fc41d76d8d1bbf0cebc8a7943bf4007335c5a7
Mounir37 <47849542+Mounir37@users.noreply.github.com>
2019-05-02 09:32:43 +0200

    Modification

---
+++ Exercice/Ex1 mise en bouche/Ex1 mise en bouche/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Ex1_mise_en_bouche
+{
+    partial class frmBienvenue
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBienvenueDate = new System.Windows.Forms.Label();
+            this.lblBienvenueTexte = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblBienvenueDate
+            // 
+            this.lblBienvenueDate.AutoSize = true;
+            this.lblBienvenueDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenueDate.Location = new System.Drawing.Point(129, 179);
+            this.lblBienvenueDate.Name = "lblBienvenueDate";
+            this.lblBienvenueDate.Size = new System.Drawing.Size(24, 20);
+            this.lblBienvenueDate.TabIndex = 0;
+            this.lblBienvenueDate.Text = "...";
+            this.lblBienvenueDate.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblBienvenueTexte
+            // 
+            this.lblBienvenueTexte.AutoSize = true;
+            this.lblBienvenueTexte.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenueTexte.Location = new System.Drawing.Point(133, 63);
+            this.lblBienvenueTexte.Name = "lblBienvenueTexte";
+            this.lblBienvenueTexte.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblBienvenueTexte.Size = new System.Drawing.Size(277, 25);
+            this.lblBienvenueTexte.TabIndex = 1;
+            this.lblBienvenueTexte.Text = "Bonjour, nous sommes le";
+            // 
+            // frmBienvenue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblBienvenueTexte);
+            this.Controls.Add(this.lblBienvenueDate);
+            this.Name = "frmBienvenue";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cours C#";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBienvenueDate;
+        private System.Windows.Forms.Label lblBienvenueTexte;
+    }
+}
+
