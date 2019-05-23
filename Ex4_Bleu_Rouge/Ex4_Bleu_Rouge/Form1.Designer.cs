da641e5158a082405a7335a8d4510689b83951d9
Mounir37 <47849542+Mounir37@users.noreply.github.com>
2019-05-23 09:34:17 +0200

    Ex4_pas fini

--- Ex4_Bleu_Rouge/Ex4_Bleu_Rouge/Form1.Designer.cs
+++ Ex4_Bleu_Rouge/Ex4_Bleu_Rouge/Form1.Designer.cs
@@ -33,16 +33,16 @@
             this.cmd_BG = new System.Windows.Forms.Button();
             this.cmd_BD = new System.Windows.Forms.Button();
             this.Bleu = new System.Windows.Forms.Button();
-            this.rouge = new System.Windows.Forms.Button();
+            this.Rouge = new System.Windows.Forms.Button();
             this.cmd_stats = new System.Windows.Forms.Button();
             this.cmd_quitter = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // cmd_HD
             // 
-            this.cmd_HD.Location = new System.Drawing.Point(434, 12);
+            this.cmd_HD.Location = new System.Drawing.Point(677, 12);
             this.cmd_HD.Name = "cmd_HD";
-            this.cmd_HD.Size = new System.Drawing.Size(126, 55);
+            this.cmd_HD.Size = new System.Drawing.Size(111, 39);
             this.cmd_HD.TabIndex = 0;
             this.cmd_HD.Text = "Haut Droite";
             this.cmd_HD.UseVisualStyleBackColor = true;
@@ -52,7 +52,7 @@
             // 
             this.cmd_HG.Location = new System.Drawing.Point(12, 12);
             this.cmd_HG.Name = "cmd_HG";
-            this.cmd_HG.Size = new System.Drawing.Size(126, 55);
+            this.cmd_HG.Size = new System.Drawing.Size(111, 39);
             this.cmd_HG.TabIndex = 1;
             this.cmd_HG.Text = "Haut Gauche";
             this.cmd_HG.UseVisualStyleBackColor = true;
@@ -60,25 +60,27 @@
             // 
             // cmd_BG
             // 
-            this.cmd_BG.Location = new System.Drawing.Point(12, 380);
+            this.cmd_BG.Location = new System.Drawing.Point(12, 398);
             this.cmd_BG.Name = "cmd_BG";
-            this.cmd_BG.Size = new System.Drawing.Size(126, 55);
+            this.cmd_BG.Size = new System.Drawing.Size(111, 39);
             this.cmd_BG.TabIndex = 2;
             this.cmd_BG.Text = "Bas Gauche";
             this.cmd_BG.UseVisualStyleBackColor = true;
+            this.cmd_BG.Click += new System.EventHandler(this.cmd_BG_Click);
             // 
             // cmd_BD
             // 
-            this.cmd_BD.Location = new System.Drawing.Point(434, 380);
+            this.cmd_BD.Location = new System.Drawing.Point(677, 398);
             this.cmd_BD.Name = "cmd_BD";
-            this.cmd_BD.Size = new System.Drawing.Size(126, 55);
+            this.cmd_BD.Size = new System.Drawing.Size(111, 39);
             this.cmd_BD.TabIndex = 3;
             this.cmd_BD.Text = "Bas Droite";
             this.cmd_BD.UseVisualStyleBackColor = true;
+            this.cmd_BD.Click += new System.EventHandler(this.cmd_BD_Click);
             // 
             // Bleu
             // 
-            this.Bleu.Location = new System.Drawing.Point(225, 83);
+            this.Bleu.Location = new System.Drawing.Point(323, 78);
             this.Bleu.Name = "Bleu";
             this.Bleu.Size = new System.Drawing.Size(126, 55);
             this.Bleu.TabIndex = 4;
@@ -86,19 +88,19 @@
             this.Bleu.UseVisualStyleBackColor = true;
             this.Bleu.Click += new System.EventHandler(this.Bleu_Click);
             // 
-            // rouge
+            // Rouge
             // 
-            this.rouge.Location = new System.Drawing.Point(225, 144);
-            this.rouge.Name = "rouge";
-            this.rouge.Size = new System.Drawing.Size(126, 55);
-            this.rouge.TabIndex = 5;
-            this.rouge.Text = "Rouge";
-            this.rouge.UseVisualStyleBackColor = true;
-            this.rouge.Click += new System.EventHandler(this.rouge_Click);
+            this.Rouge.Location = new System.Drawing.Point(323, 139);
+            this.Rouge.Name = "Rouge";
+            this.Rouge.Size = new System.Drawing.Size(126, 55);
+            this.Rouge.TabIndex = 5;
+            this.Rouge.Text = "Rouge";
+            this.Rouge.UseVisualStyleBackColor = true;
+            this.Rouge.Click += new System.EventHandler(this.Rouge_Click);
             // 
             // cmd_stats
             // 
-            this.cmd_stats.Location = new System.Drawing.Point(225, 244);
+            this.cmd_stats.Location = new System.Drawing.Point(323, 239);
             this.cmd_stats.Name = "cmd_stats";
             this.cmd_stats.Size = new System.Drawing.Size(126, 55);
             this.cmd_stats.TabIndex = 6;
@@ -107,7 +109,7 @@
             // 
             // cmd_quitter
             // 
-            this.cmd_quitter.Location = new System.Drawing.Point(225, 331);
+            this.cmd_quitter.Location = new System.Drawing.Point(323, 326);
             this.cmd_quitter.Name = "cmd_quitter";
             this.cmd_quitter.Size = new System.Drawing.Size(126, 55);
             this.cmd_quitter.TabIndex = 7;
@@ -118,10 +120,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(572, 447);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.cmd_quitter);
             this.Controls.Add(this.cmd_stats);
-            this.Controls.Add(this.rouge);
+            this.Controls.Add(this.Rouge);
             this.Controls.Add(this.Bleu);
             this.Controls.Add(this.cmd_BD);
             this.Controls.Add(this.cmd_BG);
@@ -129,6 +131,8 @@
             this.Controls.Add(this.cmd_HD);
             this.Name = "Form_fenetre";
             this.Text = "fenetre";
+            this.Load += new System.EventHandler(this.Form_fenetre_Load);
+            this.Resize += new System.EventHandler(this.Form_fenetre_Resize);
             this.ResumeLayout(false);
 
         }
@@ -140,7 +144,7 @@
         private System.Windows.Forms.Button cmd_BG;
         private System.Windows.Forms.Button cmd_BD;
         private System.Windows.Forms.Button Bleu;
-        private System.Windows.Forms.Button rouge;
+        private System.Windows.Forms.Button Rouge;
         private System.Windows.Forms.Button cmd_stats;
         private System.Windows.Forms.Button cmd_quitter;
     }
--- Ex4_Bleu_Rouge/Ex4_Bleu_Rouge/Form1.cs
+++ Ex4_Bleu_Rouge/Ex4_Bleu_Rouge/Form1.cs
@@ -8,10 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Ex4_Bleu_Rouge
 {
+    
+   
     public partial class Form_fenetre : Form
     {
+        const int dist = 12 ;
+        const int larg_cmd = 111;
+        const int haut_cmd = 39;
+        int Max_Y = scr.Width - 16; //a finir 
+        int Max_X = scr.Width - 40;
+        Rectangle scr;
+        int X;
+        int Y;
+        int X_cmd;
+        int Y_cmd;
         public Form_fenetre()
         {
             InitializeComponent();
@@ -22,23 +35,50 @@ namespace Ex4_Bleu_Rouge
             this.BackColor = Color.Blue;
         }
 
+        private void Rouge_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+        }
+
+        private void Form_fenetre_Load(object sender, EventArgs e)
+        {
+            scr = Screen.GetBounds(this);
+            X = scr.Width - this.Width;
+            Y = scr.Height - this.Height;
+        }
+
         private void cmd_HG_Click(object sender, EventArgs e)
         {
             this.Location = new Point(-6, 0);
         }
 
-        private void rouge_Click(object sender, EventArgs e)
-        {
-            this.BackColor = Color.Red;
-        }
-
         private void cmd_HD_Click(object sender, EventArgs e)
         {
-            this.Location = new Point(1338, 0);
+            this.Location = new Point(X + 6, 0);
         }
-        private void cmd_BD_Click(object sender, EventArgs e) // marche pas
+
+        private void cmd_BG_Click(object sender, EventArgs e)
         {
-            this.Location = new Point(1338, 601);
+            this.Location = new Point(-6, Y + 6);
+        }
+
+        private void cmd_BD_Click(object sender, EventArgs e)
+        {
+            this.Location = new Point(X + 6, Y + 6);
+        }
+
+        private void Form_fenetre_Resize(object sender, EventArgs e)
+        {
+            X = scr.Width - this.Width;
+            Y = scr.Height - this.Height;
+            X_cmd = scr.Width - larg_cmd - dist;
+            Y_cmd = scr.Height - larg_cmd - dist;
+
+            cmd_HG.Location = new Point(dist,dist);
+            cmd_BG.Location = new Point(dist,Y_cmd);
+            cmd_HD.Location = new Point(X_cmd, dist);
+            cmd_BD.Location = new Point(X_cmd, Y_cmd);
+
         }
     }
 }
