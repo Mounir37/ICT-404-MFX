377999f15dcc0167c5baacad8add4cf86d2d4cf7
Mounir37 <47849542+Mounir37@users.noreply.github.com>
2019-06-13 09:34:56 +0200

    Ex6_traban

---
+++ Ex6_Traban/Ex6_Traban/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Ex6_Traban
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_prix_base = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_prix_base
+            // 
+            this.lbl_prix_base.AutoSize = true;
+            this.lbl_prix_base.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_prix_base.Location = new System.Drawing.Point(23, 42);
+            this.lbl_prix_base.Name = "lbl_prix_base";
+            this.lbl_prix_base.Size = new System.Drawing.Size(380, 25);
+            this.lbl_prix_base.TabIndex = 0;
+            this.lbl_prix_base.Text = "Prix de base en € (doit être >10000 €):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(409, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_prix_base);
+            this.Name = "Form1";
+            this.Text = "Traban 2000";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_prix_base;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
